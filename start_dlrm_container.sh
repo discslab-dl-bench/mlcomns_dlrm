3aab200a908fd6764b685c67f80668a7cf6ddecd
“zhongjie-wu” <vivekwuca@gmail.com>
2022-09-04 13:15:58 -0400

    start trying different cases with gpu

--- start_dlrm_container.sh
+++ start_dlrm_container.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # mkdir results
-docker run -it --gpus all -v /raid/data/dlrm/kaggle:/data_kaggle dlrm:test /bin/bash
+docker run -it \
+--gpus all \
+# The following line determines which dataset to be used by the container
+-v /raid/data/dlrm/kaggle_16GB_Case1:/data_kaggle \
+-v $(pwd)/input:/code/input \
+dlrm:test \
+/bin/bash
