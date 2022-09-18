e21df9aed7eab3896b9192c7b3ae149721e9b347
zhongjie-wu <vivekwuca@gmail.com>
2022-09-18 17:08:05 -0400

    launch scripts modified

--- run_in_container.sh
+++ run_in_container.sh
@@ -16,6 +16,7 @@ echo "---Start running dlrm at $start_fmt---"
 
 python dlrm_s_pytorch.py \
 --use-gpu \
+--dataset-multiprocessing \
 --arch-sparse-feature-size=16 \
 --arch-mlp-bot="13-512-256-64-16" \
 --arch-mlp-top="512-256-1" \
@@ -32,7 +33,28 @@ python dlrm_s_pytorch.py \
 --mlperf-logging \
 --mlperf-bin-loader \
 --mlperf-bin-shuffle \
---test-num-workers=16 $dlrm_extra_option 2>&1 | tee /code/input/run_kaggle_mlperf.log
+--test-num-workers=16 $dlrm_extra_option 2>&1 | tee /code/input/run_kaggle_mlperf_tV2.log
+
+# python dlrm_s_pytorch.py \
+# --use-gpu \
+# --dataset-multiprocessing \
+# --arch-sparse-feature-size=16 \
+# --arch-mlp-bot="13-512-256-64-16" \
+# --arch-mlp-top="512-256-1" \
+# --data-generation=dataset \
+# --data-set=kaggle \
+# --raw-data-file=/data_kaggle/train42_200g.txt \
+# --processed-data-file=/data_kaggle/kaggleAdDisplayChallenge_processed_200g.npz \
+# --loss-function=bce \
+# --round-targets=True \
+# --learning-rate=0.1 \
+# --mini-batch-size=128 \
+# --print-freq=1024 \
+# --test-mini-batch-size=16384 \
+# --mlperf-logging \
+# --mlperf-bin-loader \
+# --mlperf-bin-shuffle \
+# --test-num-workers=16 $dlrm_extra_option 2>&1 | tee /code/input/run_kaggle_mlperf_200g.log
 
 # end timing
 end=$(date +%s)
--- start_dlrm_training.sh
+++ start_dlrm_training.sh
@@ -6,6 +6,7 @@
 
 sudo docker run -it \
 --gpus all \
+--name train_explore \
 -v /raid/data/dlrm/kaggle:/data_kaggle \
 -v /raid/data/dlrm/dlrm_logs/runtime_logs:/code/input \
 -v /dl-bench/zhongjie/mlcomns_dlrm/output:/mlperf_saving_spot \
