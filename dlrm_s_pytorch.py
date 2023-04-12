fixed formula

dlrm_s_pytorch.py
@@ -1794,6 +1794,9 @@ def run():
 
     logging.info(f"{utcnow()} Starting Training")
 
+    sleep_time_avg = np.dot([6.31850988e-03, 1.47435947e-06], [ndevices, args.mini_batch_size]) - 0.0035671384995684535
+    sleep_time_std = max(np.dot([3.35919846e-04, 6.32267933e-08], [ndevices, args.mini_batch_size]) - 0.0013904127910698847, 0)
+
     ext_dist.barrier()
     with torch.autograd.profiler.profile(args.enable_profiling, use_cuda=False, with_stack=True) as prof:
         if not args.inference_only:
@@ -1846,9 +1849,8 @@ def run():
                     log_end(key="load_batch_mem", value={"duration": perf_counter_ns() - t0})
 
                     t_compute = t0 = perf_counter_ns()
-                    
-                    sleep_time_avg = SLEEP_TIMES[str(ndevices)][str(args.mini_batch_size)]['mean']
-                    sleep_time_std = SLEEP_TIMES[str(ndevices)][str(args.mini_batch_size)]['std']
+                            
+
                     time.sleep(random.normal(sleep_time_avg, sleep_time_std))
                     
                     # if args.mlperf_logging:

generate_data.py
@@ -15,7 +15,7 @@ CATEGORY_RANGES = [10000000,    38985,    17278,     7418,    20242,        3,
 def generate_data_dlrm_bin(output_path, desired_size): 
     total_size = 0
     while total_size < desired_size: 
-        fn = f"{output_path}/preprocessed.bin"
+        fn = f"{output_path}/terabyte_processed_train.bin"
         with open(fn, 'ab') as output_file:
             num_instance = 6548660
             X_int = np.random.randint(2557264, size = (num_instance, 13))
@@ -24,7 +24,8 @@ def generate_data_dlrm_bin(output_path, desired_size):
             np_data = np.concatenate([y.reshape(-1, 1), X_int, X_cat], axis=1)
             np_data = np_data.astype(np.int32)
             output_file.write(np_data.tobytes())
-        total_size += os.path.getsize(fn)
+            output_file.flush()
+        total_size = os.path.getsize(fn)
         print("current size", total_size)
 
 