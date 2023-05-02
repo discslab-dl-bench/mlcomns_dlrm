8c35f08d5cde40ab2e2b5be658c07ed9fdcc3feb
jovonho <loic.hovon@gmail.com>
2023-05-02 17:46:30 -0400

    cleanup

--- start_training.sh
+++ start_training.sh
@@ -2,13 +2,11 @@
 
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 
-# TERABYTE_PROC_DIR="/raid/data/dlrm/terabyte_mmap_bin"
-DATA_DIR="/dl-bench/lhovon/mlcomns_dlrm/data"
+# Change these directories
+DATA_DIR="$SCRIPT_DIR/data"
+OUTPUT_DIR="$SCRIPT_DIR/output"
+LOGGING_DIR="$SCRIPT_DIR/logs"
 
-# Default output dir will be where this script is located
-OUTPUT_DIR="/raid/data/dlrm/run_output"
-
-LOGGING_DIR=${3:-"$SCRIPT_DIR/output"}
 
 NUM_GPUS=${1:-8}
 CONTAINER_NAME=${2:-train_dlrm}
