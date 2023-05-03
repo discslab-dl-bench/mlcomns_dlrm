0df7125269f0f1304d61bcd1c168820a5469c81f
jovonho <loic.hovon@gmail.com>
2023-05-03 19:19:27 -0400

    add logging dir as argument to launch script

--- start_training.sh
+++ start_training.sh
@@ -5,12 +5,11 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 # Change these directories
 DATA_DIR="$SCRIPT_DIR/data"
 OUTPUT_DIR="$SCRIPT_DIR/output"
-LOGGING_DIR="$SCRIPT_DIR/logs"
-
 
 NUM_GPUS=${1:-8}
 CONTAINER_NAME=${2:-train_dlrm}
-IMAGE_NAME=${4:-dlrm:instrumented}
+LOGGING_DIR=${3:-"$SCRIPT_DIR/output"}
+IMAGE_NAME=${4:-dlrm:sleep}
 BATCH_SIZE=${5:-32768}
 BATCH_SIZE_EVAL=${6:-16384}
 NUM_BATCHES=${7:-32768}
@@ -22,4 +21,3 @@ docker run -it --rm --gpus $NUM_GPUS --name $CONTAINER_NAME \
     -v $LOGGING_DIR:/code/output \
     -v $OUTPUT_DIR:/code/ckpts \
     $IMAGE_NAME /bin/bash run_terabyte_mmap_bin.sh $BATCH_SIZE $BATCH_SIZE_EVAL $NUM_BATCHES $NUM_BATCHES_EVAL
-
